84b21916b0cff026e994a9d47960cb74df5ae5c6
Iris Teyssier <iris.teyssier@gmail.com>
2024-03-01 21:42:31 -0800

    various

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,61 @@
-use {args::Args, std::{error::Error, io}};
+use {
+    args::Args,
+    colored::Colorize,
+    std::{error::Error, io},
+};
 
 pub mod args;
-pub mod text;
-pub mod parse;
 pub mod board;
+pub mod parse;
+pub mod text;
 
 pub fn run(_args: Args) {
+    // println!("control");
+    // println!("{}", "blue".blue());
+    // println!("{}", "red on blue".red().on_blue());
+    // println!("{}", "bold".bold());
+    // println!(
+    //     "{} {} {}",
+    //     "cyan".cyan(),
+    //     "italic yellow".italic().yellow(),
+    //     "cyan".cyan()
+    // );
+    // println!("{}", "yellow blue red".yellow().blue().red());
+    // println!(
+    //     "{}",
+    //     "bright blue on bright white"
+    //         .bright_blue()
+    //         .on_bright_white()
+    // );
+
     println!("{}", text::intro);
 
     let mut input = String::new();
 
     // move:
-        // move a piece
-        // usage:
-            // `move [piece-name] [position]`
-            // `move [position] [position]`
-        // if only one piece of that name can move there, move it
-        // otherwise print error requiring `move [position] [position]` format
-    
+    // move a piece
+    // usage:
+    // `move [piece-name] [position]`
+    // `move [position] [position]`
+    // if only one piece of that name can move there, move it
+    // otherwise print error requiring `move [position] [position]` format
+
     // check:
-        // see the effect of moving a piece (without actually moving it)
-        // usage:
-            // `move [piece-name] [position]`
-            // `move [position] [position]`
-    
+    // see the effect of moving a piece (without actually moving it)
+    // usage:
+    // `move [piece-name] [position]`
+    // `move [position] [position]`
+
     // hint:
-        // get a (questionable) hint on where to move
-        // usage:
-            // `hint`
-    
-
-
+    // get a (questionable) hint on where to move
+    // usage:
+    // `hint`
 }
 
 fn wait_for_enter(input: &mut String) -> Result<(), Box<dyn Error>> {
     println!("Press enter to continue . . .");
     get_input(input)?;
+    Ok(())
 }
 
 fn get_input(buf: &mut String) -> Result<(), Box<dyn Error>> {
@@ -43,5 +63,6 @@ fn get_input(buf: &mut String) -> Result<(), Box<dyn Error>> {
     //     println!("Unexpected input error: {}", err);
     //     process::exit(1);
     // });
-    io::stdin().read_line(buf)
+    io::stdin().read_line(buf)?;
+    Ok(())
 }
