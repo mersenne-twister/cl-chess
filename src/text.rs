bff5fa58d7c901ee1ba0a76ebf832bceb0e66f0a
Iris Teyssier <iris.teyssier@gmail.com>
2024-03-03 08:47:39 -0800

    add help

--- src/text.rs
+++ src/text.rs
@@ -1,8 +1,24 @@
 pub const INTRO: &str = "\
 Welcome to cl-chess, made by Iris Teyssier!
-iris.teyssier@gmail.com
-
-commands:";
+iris.teyssier@gmail.com";
 
 pub const HELP: &str = "\
-";
+move:
+move a piece
+usage:
+    `move [piece-name] [position]`
+    `move [position] [position]`
+if only one piece of that name can move there, move it
+otherwise print error requiring `move [position] [position]` format
+UNAMBIGOUS
+
+check:
+see the effect of moving a piece (without actually moving it)
+usage:
+    `move [piece-name] [position]`
+    `move [position] [position]`
+
+hint:
+get a (questionable) hint on where to move
+usage:
+  `hint`";
