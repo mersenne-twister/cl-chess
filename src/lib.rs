f06c423b23f5d37d646baa71dd23d09ffd32f0d9
Iris Teyssier <iris.teyssier@gmail.com>
2024-03-02 20:33:09 -0800

    add note

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,7 @@ pub fn run(_args: Args) -> Result<(), Box<dyn Error>> {
     // `move [position] [position]`
     // if only one piece of that name can move there, move it
     // otherwise print error requiring `move [position] [position]` format
+    // UNAMBIGOUS
 
     // check:
     // see the effect of moving a piece (without actually moving it)
