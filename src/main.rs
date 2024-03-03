629a04393e8a9687f026447b6504b60846354619
Iris Teyssier <iris.teyssier@gmail.com>
2024-03-02 16:43:20 -0800

    add error propagation

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use {cl_chess::args::Args, clap::Parser};
 fn main() {
     let args = Args::parse();
 
-    cl_chess::run(args);
+    cl_chess::run(args).unwrap_or_else(|err| {
+        println!("Unexpected error: {}", err);
+    });
 }
