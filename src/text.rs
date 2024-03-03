059336627075330c02322eab8e02ec5dbca630ca
Iris Teyssier <iris.teyssier@gmail.com>
2024-03-03 13:05:08 -0800

    add help

--- src/text.rs
+++ src/text.rs
@@ -8,17 +8,18 @@ move a piece
 usage:
     `move [piece-name] [position]`
     `move [position] [position]`
-if only one piece of that name can move there, move it
+if intended piece is unambigous, move it
 otherwise print error requiring `move [position] [position]` format
-UNAMBIGOUS
 
 check:
 see the effect of moving a piece (without actually moving it)
 usage:
-    `move [piece-name] [position]`
-    `move [position] [position]`
+    `check [piece-name] [position]`
+    `check [position] [position]`
 
 hint:
 get a (questionable) hint on where to move
 usage:
-  `hint`";
+  `hint`
+  
+To print the board, enter without typing anything.";
