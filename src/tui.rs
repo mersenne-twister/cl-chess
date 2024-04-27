6dd263dfd10e7dffa42847573ecc27ad8dd11066
Iris Teyssier <iris.teyssier@gmail.com>
2024-04-27 12:54:05 -0700

    add tui hello world

--- src/tui.rs
+++ src/tui.rs
@@ -1,21 +1,47 @@
 use {
-    crate::args::Args,
-    crossterm::{
-        event::{self, KeyCode, KeyEventKind},
+    crate::args::Args, core::time, crossterm::{
+        event::{self, Event, KeyCode, KeyEventKind},
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
         ExecutableCommand,
-    },
-    ratatui::{
+    }, ratatui::{
         prelude::{CrosstermBackend, Stylize, Terminal},
         widgets::Paragraph,
-    },
-    std::{error::Error, io::stdout},
+    }, std::{time::Duration, error::Error, io::stdout}
 };
 
 pub fn run_tui(args: Args) -> Result<(), Box<dyn Error>> {
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
 
+    // main loop
+    // encapsulate this later
+    loop {
+        //draw ui
+        terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+                Paragraph::new("Hello world! (press 'q' to quit)")
+                .white()
+                .on_blue(),
+            area,
+            );
+        })?;
+
+        //handle events
+        if event::poll(time::Duration::from_millis(16))? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press
+                && key.code == KeyCode::Char('q')
+            {
+                break;
+            }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
     Ok(())
 }
